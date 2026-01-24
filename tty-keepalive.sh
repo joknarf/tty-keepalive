ee343cd8c571af2c353f6ef39dc515ff38906f9e
joknarf <joknarf@free.fr>
2026-01-24 14:36:38 +0100

    silent pgrep if not avail

--- tty-keepalive.sh
+++ tty-keepalive.sh
@@ -24,13 +24,13 @@ PTS=$(tty 2>/dev/null)
 [ "$PTS" ] || exit 1
 [ -w "$PTS" ] || exit 1
 printf "$TTY_CODE" 2>/dev/null >"$PTS" || exit 0
-PIDS=( $(pgrep -t ${PTS#/dev/} -x tty-keepalive) )
+PIDS=( $(pgrep -t ${PTS#/dev/} -x tty-keepalive 2>/dev/null) )
 (( ${#PIDS[@]} > 1 )) && exit 
 (
   trap exit SIGHUP
   while :; do
     sleep $SLEEP
-    [ -e "$PTS" ] || break
+    [ -w "$PTS" ] || break
     printf "$TTY_CODE" >"$PTS"
   done
 ) >/dev/null 2>&1 </dev/null &
