ce3a74d502624bce4892b1fdd1a6bb2f846ecf6a
joknarf <joknarf@free.fr>
2026-01-24 13:38:37 +0100

    exit if not tty owner

--- tty-keepalive.sh
+++ tty-keepalive.sh
@@ -19,14 +19,17 @@
 [ "$1" = -h ] && echo "usage: ${0##/} [<SECONDS>]" && exit 0
 [ -t 1 ] || exit 0
 SLEEP=${1-120}
-PTS=$(tty)
+PTS=$(tty 2>/dev/null)
+[ "$PTS" ] || exit 1
+TTY_CODE='\033]9;0\a' # Visual Bell
+printf "$TTY_CODE" 2>/dev/null >"$PTS" || exit 0
 PIDS=( $(pgrep -t ${PTS#/dev/} -x tty-keepalive) )
-(( ${#PIDS[@]} > 1 )) && exit 1
+(( ${#PIDS[@]} > 1 )) && exit 
 (
   trap exit SIGHUP
   while :; do
     sleep $SLEEP
     [ -e "$PTS" ] || break
-    printf '\033]9;0\a' >"$PTS"
+    printf "$TTY_CODE" >"$PTS"
   done
-) &
+) >/dev/null 2>&1 </dev/null &
